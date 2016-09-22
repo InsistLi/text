14561aac27b892f4e21b430ec9c01ae34fdca404
Rocky <wufeiqun_cn@126.com>
2016-09-22 17:55:20 +0800

    first commit

---
+++ reverse_string/reverse_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+def reverse_string(args):
+    ret = ""
+    for x in range(-1, -len(args)-1, -1):
+        ret+=args[x]
+    return ret
+
+if __name__ == "__main__":
+    args = "Hi,Rocky!"
+    print reverse_string(args)
